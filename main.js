785e7a5f9c76f520063a51105f155895d6f4ff9d
Vasya1988 <myhappy1988@gmail.com>
2021-01-12 17:04:08 +0300

    Update_2

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const items = {
     menu: document.querySelector('.menu-list')
 }
 
+const images = [
+    {
+        name: 'Iphone',
+        description: `Год выпуска: 2007 <br>
+        Объем памяти: 4, 8, 16 ГБ <br>
+        Номер модели на задней панели корпуса: A1203.`,
+        info: 'Сведения: задняя панель корпуса изготовлена из анодированного алюминия. С верхней стороны имеется лоток SIM-карты для карт второго форм-фактора (2FF) Mini-SIM. Серийный номер выгравирован на задней панели корпуса'
+    }
+]
+
 console.log(items.menu);
 
 items.burger.addEventListener('click', (event)=> {
